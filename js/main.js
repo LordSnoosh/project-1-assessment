fc118c1c888d8ee8d785aa7435186fae7f1651b6
github_username <lordsnoosh@gmail.com>
2021-03-11 16:05:34 -0800

    JS work

--- js/main.js
+++ js/main.js
@@ -5,14 +5,15 @@ var totalDisp = {
 };
 
 totalDisp.totalvalue = document.getElementById("number-input");
-document.getElementById("plus").addEventListener("click", addition);
-document.getElementById("minus").addEventListener("click", subtraction);
+document.getElementById("plusId").addEventListener("click", addition);
+document.getElementById("minusId").addEventListener("click", subtraction);
 
 function addition() {
 var inptval = parseInt(document.getElementById("number-input").value);
 var initval = totalDisp.initvalue;
 var totl = document.getElementById("count-display")
 totl.value = initval = inptval;
+console.log(totl);
 };
 
 function subtraction() {
