9f7e55c5c27120b71056b663204bd37e55a5499e
github_username <lordsnoosh@gmail.com>
2021-03-11 17:34:35 -0800

    js

--- js/main.js
+++ js/main.js
@@ -13,13 +13,13 @@ function initsub() {
     };
 
 
-totalDisp.totalvalue = document.getElementById("disp").value;
-totalDisp.inputnumber = document.getElementById("input").value;
+totalDisp.totalvalue = document.getElementById("disp").innerHTML;
+totalDisp.inputnumber = document.getElementById("input").innerHTML;
 document.getElementById("plusId").addEventListener("click", addition);
 document.getElementById("minusId").addEventListener("click", subtraction);
 
 function addition() {
-var inptval = parseInt(document.getElementById("input").value);
+var inptval = parseInt(document.getElementById("input").innerHTML);
 var initval = parseInt(totalDisp.initvalue);
 var result = inptval + initval;
 document.getElementById("disp").innerHTML = result;
@@ -28,7 +28,7 @@ initadd();
 
 function subtraction() {
     var total = parseInt(document.getElementById("disp").innerHTML);
-    var val2 = parseInt(document.getElementById("input").value);
+    var val2 = parseInt(document.getElementById("input").innerHTML);
     var result = total - val2;
     document.getElementById("disp").innerHTML = result;
     initsub();
