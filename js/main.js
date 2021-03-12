3c3a07789aa8fcb44c57cd7585a32c1ec543e424
github_username <lordsnoosh@gmail.com>
2021-03-11 18:06:52 -0800

    refactor simplistic format

--- js/main.js
+++ js/main.js
@@ -1,37 +1,35 @@
-// App State
-let runningTotal;
-// Cached Elements
-const input1El = document.querySelector("#num2");
+/*----- app's state (variables) -----*/
+let total;
+/*----- cached element references -----*/
+const inputEl = document.querySelector("#one");
 const totalEl = document.querySelector("h1 span");
-// Event Listeners
-document.querySelector("#plus").addEventListener("click", handleClick);
-document.querySelector("#minus").addEventListener("click", handleClick);
-
-// Functions
-
-
-
+/*----- event listeners -----*/
+document.getElementById("plus").addEventListener("click", handleAdd);
+document.getElementById("minus").addEventListener("click", handleSub);
+/*----- functions -----*/
 function init() {
-  runningTotal = 0;
-  render();
-};
-
+    total = 0;
+    render();
+}
 init();
-
 function render() {
-  if (runningTotal >= 0) {
-    totalEl.textContent = "+" + runningTotal;
-    totalEl.style.color = "green";
-  } else {
-    totalEl.textContent = runningTotal;
-    totalEl.style.color = "red";
-  }
-};
-
-function handleClick() {
-  let input1Value = parseInt(input1El.value);
-  let input3Value = parseInt(runningTotal.value);
-  total += (input1Value + input3Value);
-  render();
-  input1El.value = input2El.value =input3El.value = "";
-};
+    if (total >= 0) {
+        totalEl.textContent = total;
+        totalEl.style.color = "green";
+        inputEl.value = "1";
+    } else {
+        totalEl.textContent = total;
+        totalEl.style.color = "red";
+        inputEl.value = "1";
+    } 
+}
+function handleAdd() {
+    let plusValue = parseInt(inputEl.value);
+    total += (plusValue);
+    render();
+ }
+ function handleSub() {
+    let minusValue = parseInt(inputEl.value);
+    total -= (minusValue);
+    render();
+ };
