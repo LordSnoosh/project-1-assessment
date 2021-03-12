d8690507d786d6a63cf3a9db6008601024c2cbb1
github_username <lordsnoosh@gmail.com>
2021-03-11 16:28:01 -0800

    js html fixes

--- js/main.js
+++ js/main.js
@@ -1,21 +1,25 @@
 var totalDisp = {
-    totalvalue: null,
+    totalvalue: [],
     initvalue: 1,
     inputnumber: ""
 };
 
-totalDisp.totalvalue = document.getElementById("number-input");
+totalDisp.totalvalue = document.getElementById("display");
+totalDisp.inputnumber = document.getElementById("number-input")
 document.getElementById("plusId").addEventListener("click", addition);
 document.getElementById("minusId").addEventListener("click", subtraction);
 
 function addition() {
 var inptval = parseInt(document.getElementById("number-input").value);
-var initval = totalDisp.initvalue;
-var totl = document.getElementById("count-display")
-totl.value = initval = inptval;
-console.log(totl);
+var initval = parseInt(totalDisp.initvalue.value);
+var result = inptval + initval;
+document.getElementById("display").value = result;
+return false;
 };
 
 function subtraction() {
-
-};
+    var inptval = parseInt(document.getElementById("number-input").value);
+    var initval = totalDisp.initvalue;
+    var minused = parseInt(inptval) - parseInt(initval);
+    
+    };
